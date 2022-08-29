5213cc79ec1af09f2f07a05acbb2e406b4152b68
Jan_Lebar <janstefanlebar@gmail.com>
2022-08-29 17:56:46 +0200

    dfga

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from email.policy import default
 from unicodedata import category
 
 # v flask importas se request, ki je potreben da nov post poveze v bazo line 32
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 # TO SPODI JE SAM DA PYTHON POVLECE NOT KNJIZNICO ZA BAZO
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -16,6 +16,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
 db = SQLAlchemy(app)
 session = db.session
 migrate = Migrate(app, db)
+
+
 
 # TO SPODI JE STRUKTURA ZA BAZO MODEL, DELA SE Z CLASSI? BAZA MA TKO K TABELA COLUMNE, NULLABLE=FALSE POMENI DA NE MORE BIT PRAZN KER CE NI KONTENTA NE MORE BIT POVSTA
 # CE AVTORJA NI GA DODA KOT N/A
@@ -37,8 +39,10 @@ class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
 
+
 # spodaj baza za blogpovste (child)
 class BlogPost(db.Model):
+    """Baza za poste"""
 
     __tablename__ = "blog_post"
     __mapper_args__ = {"eager_defaults": True}
@@ -56,18 +60,16 @@ class BlogPost(db.Model):
     category = db.relationship('Category',
         backref=db.backref('work_type', lazy=True))
 
-
-
-
-
-
-# KREIRA OBJEKT V BAZI IN DA ID
     def __repr__(self):
+        """vrne objekt reprezentativni"""
         return 'Blog post ' + str(self.id)
+
+
 # DA NAREDIS BAZO GRES V TERMINAL NA LOKACIJO KJER BO IN NAPISES FROM APP IMPORT DB, KASNEJE DB.CREATE_ALL()
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/posts', methods=['GET', 'POST'])
 def posts():
@@ -80,17 +82,18 @@ def posts():
         post_category_id = request.form["category"]
         new_post = BlogPost(title=post_title, content=post_content, offer=post_offer, email=post_email, category_id=post_category_id)
 
-
-# vpise v bazo v trenutno
+        # vpise v bazo v trenutno
         db.session.add(new_post)
-# commit ga sele vpise permanentno v bazo
+        # commit ga sele vpise permanentno v bazo
         db.session.commit()
-# vrne posodobljen posts page
+        # vrne posodobljen posts page
         return redirect('/posts')
     else:
-# drugace vrne prejsnje povste urejene po datumu query.order_by date_posted
+        # drugace vrne prejsnje povste urejene po datumu query.order_by date_posted
         all_posts = BlogPost.query.order_by(BlogPost.date_posted).all()
         return render_template('posts.html', posts=all_posts)
+
+
 # rout za delete post
 @app.route('/posts/delete/<int:id>')
 def delete(id):
@@ -98,7 +101,9 @@ def delete(id):
     db.session.delete(post)
     db.session.commit()
     return redirect('/posts')
-# rout za edit post, ker ga urejas mora bit metoda post ker jo shrani v bazo
+
+
+# route za edit post, ker ga urejas mora bit metoda post ker jo shrani v bazo
 @app.route('/posts/edit/<int:id>', methods=['GET', 'POST'])
 def edit(id):
     
@@ -109,27 +114,35 @@ def edit(id):
         post.offer = request.form['offer']
         post.content = request.form['content']
         post.email = request.form['email']
+        post.category = request.form ['category']
         db.session.commit()
         return redirect('/posts')
     else:
-# post=post ker rabi prebrisat prejsn povst
+        # post=post ker rabi prebrisat prejsn povst
         return render_template('edit.html', post=post)
+
 
 @app.route('/posts/new', methods=['GET', 'POST'])
 def new_post():
-    if request.method == 'POST':
-        post.title = request.form['title']
-        post.offer = request.form['offer']
-        post.content = request.form['content']
-        post.email = request.form['email']
-        new_post = BlogPost(title=post_title, content=post_content, offer=post_offer, email=post_email)
-        db.session.add(new_post)
-        db.session.commit()
-        return redirect('/posts')
-    else:
-# jaka naredil da naredi categorije poizvedbo za vse 
+    # if request.method == 'POST':
+    #     post.title = request.form['title']
+    #     post.offer = request.form['offer']
+    #     post.content = request.form['content']
+    #     post.email = request.form['email']
+
+    #     new_post = BlogPost(title=post_title, content=post_content, offer=post_offer, email=post_email)
+    #     db.session.add(new_post)
+    #     db.session.commit()
+    #     return redirect('/posts')
+    # else:
+        # jaka naredil da naredi categorije poizvedbo za vse 
         categories = Category.query.all()
-        return render_template('new_post.html', categories=categories)
+        return render_template('new_post.html', categories=categories, action_url=url_for(posts.__name__))
+
+
+
+
+
 
 # jan naredil podstran
 @app.route('/about', methods=['GET', 'POST'])
