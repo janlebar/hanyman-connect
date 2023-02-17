a21c5332f7a1c3948d4c9eb31c6df5f7b31fcefe
Jan_Lebar <janstefanlebar@gmail.com>
2023-02-17 12:30:28 +0100

    jasonify added

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from secrets import randbelow
 from itsdangerous import URLSafeTimedSerializer, SignatureExpired
 # flask needs to import request, to get data from database
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_migrate import Migrate
 from flask_mail import Mail
 from flask_mail import Message
