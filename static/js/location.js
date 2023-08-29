c14e0641f16e95fe1ef5f5451ea971bb8cae39fe
Jan_Lebar <janstefanlebar@gmail.com>
2023-08-29 17:05:57 +0200

    debugging location

--- static/js/location.js
+++ static/js/location.js
@@ -1,26 +1,32 @@
-  window.onload = function() {
+window.onload = function() {
     const status = document.querySelector('.status');
 
-
-
-
     const dispatchLocation = (longitude, latitude) => {
-        const event = new CustomEvent("location", {detail: { latitude, longitude }});
-
+        const event = new CustomEvent("location", { detail: { latitude, longitude } });
         document.dispatchEvent(event);
     };
 
     const success = (position) => {
         dispatchLocation(position.coords.longitude, position.coords.latitude);
-    }
+    };
 
     const error = () => {
         status.textContent = 'Unable to retrieve your location';
-    }
+    };
 
     if (!longitude || !latitude) {
         navigator.geolocation.getCurrentPosition(success, error);
     } else {
         dispatchLocation(longitude, latitude);
     }
+    
+    document.addEventListener("location", (event) => {
+        document.getElementById("latitude").value = event.detail.latitude;
+        document.getElementById("longitude").value = event.detail.longitude;
+
+        // Store the location in the session
+        sessionStorage.setItem('latitude', event.detail.latitude);
+        sessionStorage.setItem('longitude', event.detail.longitude);
+    });
 };
+
